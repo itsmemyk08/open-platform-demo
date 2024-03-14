7b8194307537f6b92b4460f9a0b0a259a6a34828
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2024-03-14 11:48:43 +0530

    Implement computeItems action
    ComputeItems method is used to modify sale order lines
    either by children or parent depending on line product updateType.

--- modules/demo-sale/src/main/java/com/axelor/sale/web/SaleOrderController.java
+++ modules/demo-sale/src/main/java/com/axelor/sale/web/SaleOrderController.java
@@ -18,153 +18,342 @@
  */
 package com.axelor.sale.web;
 
-import com.axelor.common.ObjectUtils;
-import com.axelor.db.JpaSupport;
-import com.axelor.i18n.I18n;
-import com.axelor.meta.schema.actions.ActionView;
-import com.axelor.rpc.ActionRequest;
-import com.axelor.rpc.ActionResponse;
-import com.axelor.sale.db.Order;
-import com.axelor.sale.db.OrderStatus;
-import com.axelor.sale.service.SaleOrderService;
-import com.google.common.collect.Lists;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import com.axelor.common.ObjectUtils;
+import com.axelor.db.EntityHelper;
+import com.axelor.db.JpaSupport;
+import com.axelor.db.mapper.Mapper;
+import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
+import com.axelor.meta.schema.actions.ActionView;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Resource;
+import com.axelor.sale.db.Order;
+import com.axelor.sale.db.OrderLine;
+import com.axelor.sale.db.OrderStatus;
+import com.axelor.sale.db.Product;
+import com.axelor.sale.db.repo.OrderLineRepository;
+import com.axelor.sale.db.repo.OrderRepository;
+import com.axelor.sale.db.repo.ProductRepository;
+import com.axelor.sale.service.SaleOrderService;
+import com.google.common.collect.Lists;
+import com.google.inject.persist.Transactional;
+
 public class SaleOrderController extends JpaSupport {
 
-  @Inject private SaleOrderService service;
+	@Inject
+	private SaleOrderService service;
 
-  public void onConfirm(ActionRequest request, ActionResponse response) {
+	// Item helpers
+	private OrderLine findOrderLine(Map<String, Object> item) {
+		int id = item.get("id") != null ? (int) item.get("id") : 0;
+		if (id > 0) {
+			OrderLine line = Beans.get(OrderLineRepository.class).find((long) id);
+			return line;
+		}
+		return null;
+	}
 
-    Order order = request.getContext().asType(Order.class);
+	private String getItemProductUpdateType(Map<String, Object> item) {
+		String updateType = "";
 
-    response.setReadonly("orderDate", order.getConfirmed());
-    response.setReadonly("confirmDate", order.getConfirmed());
+		Map<String, Object> product = ((Map<String, Object>) item.get("product"));
+		if (product != null) {
+			int productId = (int) product.get("id");
+			Product lineProduct = Beans.get(ProductRepository.class).find((long) productId);
 
-    if (order.getConfirmed() == Boolean.TRUE && order.getConfirmDate() == null) {
-      response.setValue("confirmDate", LocalDate.now());
-    }
+			if (lineProduct != null && lineProduct.getUpdateType() != null && !lineProduct.getUpdateType().isEmpty()) {
+				updateType = lineProduct.getUpdateType();
+			}
+		}
 
-    if (order.getConfirmed() == Boolean.TRUE) {
-      response.setValue("status", OrderStatus.OPEN);
-    } else if (order.getStatus() == OrderStatus.OPEN) {
-      response.setValue("status", OrderStatus.DRAFT);
-    }
-  }
+		return updateType;
+	}
 
-  public void calculate(ActionRequest request, ActionResponse response) {
+	private int getItemQuantity(Map<String, Object> item) {
+		if (item.get("quantity") != null) {
+			return (int) item.get("quantity");
+		}
+		OrderLine line = findOrderLine(item);
+		return line != null ? line.getQuantity() : 0;
+	}
 
-    Order order = request.getContext().asType(Order.class);
-    order = service.calculate(order);
+	private boolean hasItemChanged(Map<String, Object> item) {
+		return item.get("_changed") != null && item.get("_changed").toString().equals("true");
+	}
 
-    response.setValue("amount", order.getAmount());
-    response.setValue("taxAmount", order.getTaxAmount());
-    response.setValue("totalAmount", order.getTotalAmount());
-  }
+	private void resetItem(Map<String, Object> item) {
+		if (item.get("_changed") != null) {
+			item.remove("_changed");
+		}
+	}
 
-  public void reportToday(ActionRequest request, ActionResponse response) {
-    EntityManager em = getEntityManager();
-    Query q1 =
-        em.createQuery(
-            "SELECT SUM(self.totalAmount) FROM Order AS self "
-                + "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
-                + "MONTH(self.orderDate) = MONTH(current_date) AND "
-                + "DAY(self.orderDate) = DAY(current_date) - 1");
+	private Object getItemOriginalField(Map<String, Object> item, String key) {
+		Map<String, Object> original = (Map<String, Object>) item.get("_original");
+		if (original != null) {
+			return original.get(key);
+		}
+		return item.get(key);
+	}
 
-    Query q2 =
-        em.createQuery(
-            "SELECT SUM(self.totalAmount) FROM Order AS self "
-                + "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
-                + "MONTH(self.orderDate) = MONTH(current_date) AND "
-                + "DAY(self.orderDate) = DAY(current_date)");
+	private float getItemQtyChangedRatio(Map<String, Object> item) {
+		Object oldQty = getItemOriginalField(item, "quantity");
+		Object qty = item.get("quantity");
 
-    List<?> r1 = q1.getResultList();
-    BigDecimal last = r1.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r1.get(0);
+		if (oldQty != null && qty != null) {
+			int _q = (int) oldQty;
+			int q = (int) qty;
+			return (float) q / _q;
+		}
 
-    List<?> r2 = q2.getResultList();
-    BigDecimal total = r2.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r2.get(0);
+		return 1;
+	}
+	// Items helpers
 
-    BigDecimal percent = BigDecimal.ZERO;
-    if (total.compareTo(BigDecimal.ZERO) == 1) {
-      percent =
-          total.subtract(last).multiply(new BigDecimal(100)).divide(total, RoundingMode.HALF_UP);
-    }
+	private List<Object> getItems(Map<String, Object> item) {
+		List<Object> subItems = new ArrayList<Object>();
 
-    Map<String, Object> data = new HashMap<>();
-    data.put("total", total);
-    data.put("percent", percent);
-    data.put("down", total.compareTo(last) == -1);
+		try {
+			subItems = (List<Object>) item.get("items");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    response.setData(Lists.newArrayList(data));
-  }
+		if (subItems == null) {
+			subItems = new ArrayList<Object>();
 
-  public void reportMonthly(ActionRequest request, ActionResponse response) {
-    EntityManager em = getEntityManager();
-    Query q1 =
-        em.createQuery(
-            "SELECT SUM(self.totalAmount) FROM Order AS self "
-                + "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
-                + "MONTH(self.orderDate) = MONTH(current_date) - 1");
+			OrderLine orderLine = findOrderLine(item);
 
-    Query q2 =
-        em.createQuery(
-            "SELECT SUM(self.totalAmount) FROM Order AS self "
-                + "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
-                + "MONTH(self.orderDate) = MONTH(current_date)");
+			if (orderLine != null && orderLine.getItems() != null) {
+				for (OrderLine subLine : orderLine.getItems()) {
+					Product product = subLine.getProduct();
 
-    List<?> r1 = q1.getResultList();
-    BigDecimal last = r1.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r1.get(0);
+					subItems.add(new HashMap<String, Object>() {
+						{
+							put("id", Integer.parseInt(subLine.getId().toString()));
+							put("version", subLine.getVersion());
+							put("price", subLine.getPrice());
+							if (product != null) {
+								put("product", Resource.toMapCompact(product));
+							}
+						}
+					});
+				}
 
-    List<?> r2 = q2.getResultList();
-    BigDecimal total = r2.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r2.get(0);
+			}
 
-    BigDecimal percent = BigDecimal.ZERO;
-    if (total.compareTo(BigDecimal.ZERO) == 1) {
-      percent = total.subtract(last).divide(total, 4, RoundingMode.HALF_UP);
-    }
+			item.put("items", subItems);
+		}
 
-    Map<String, Object> data = new HashMap<>();
-    data.put("total", total);
-    data.put("percent", percent);
-    data.put("up", total.compareTo(last) > 0);
-    data.put("tag", I18n.get("Monthly"));
-    data.put("tagCss", "label-success");
+		return subItems;
+	}
 
-    response.setData(Lists.newArrayList(data));
-  }
+	private ArrayList<Map<String, Object>> flattenItems(Map<String, Object> item) {
+		ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		list.add(item);
 
-  public void showTotalSales(ActionRequest request, ActionResponse response) {
-    List<Map<String, Object>> data =
-        (List<Map<String, Object>>) request.getRawContext().get("_data");
-    if (ObjectUtils.isEmpty(data)) {
-      response.setNotify(I18n.get("No sales"));
-      return;
-    }
-    BigDecimal totalAmount =
-        data.stream()
-            .map(i -> i.get("amount").toString())
-            .map(BigDecimal::new)
-            .reduce(BigDecimal.ZERO, BigDecimal::add);
-    response.setNotify(String.format("%s : %s", I18n.get("Total sales"), totalAmount));
-  }
+		ArrayList<Map<String, Object>> subItems = (ArrayList<Map<String, Object>>) item.get("items");
 
-  public void showCustomerSales(ActionRequest request, ActionResponse response) {
-    Object data = request.getRawContext().get("customerId");
-    if (ObjectUtils.isEmpty(data)) {
-      return;
-    }
+		if (subItems != null) {
+			for (Map<String, Object> subItem : subItems) {
+				list.addAll(flattenItems(subItem));
+			}
+		}
+		return list;
+	}
 
-    ActionView.ActionViewBuilder builder =
-        ActionView.define("Customer sales").model(Order.class.getName());
-    builder.domain("self.customer.id = " + data);
-    response.setView(builder.map());
-  }
+	// Quantity updaters
+
+	private int updateQuantityByParent(Map<String, Object> item) {
+		if (!hasItemChanged(item)) {
+			int totalQty = 0;
+			ArrayList<Map<String, Object>> subItems = (ArrayList<Map<String, Object>>) item.get("items");
+			if (subItems != null && subItems.size() > 0) {
+				boolean anySubItemChanged = subItems.stream().anyMatch(_item -> hasItemChanged(_item));
+				for (Map<String, Object> subItem : subItems) {
+					int qty = anySubItemChanged ? getItemQuantity(subItem) : updateQuantityByParent(subItem);
+					totalQty += qty;
+
+					subItem.put("quantity", qty);
+					resetItem(subItem);
+				}
+			} else {
+				totalQty = getItemQuantity(item);
+			}
+			item.put("quantity", totalQty);
+			return totalQty;
+		}
+		resetItem(item);
+		return (int) item.get("quantity");
+	}
+
+	private void updateQuantityByChildren(Map<String, Object> item, float ratio) {
+		for (Object _subItem : getItems(item)) {
+			Map<String, Object> subItem = (Map<String, Object>) _subItem;
+			subItem.put("version", subItem.get("version"));
+			subItem.put("quantity", Math.round(getItemQuantity(subItem) * ratio));
+			updateQuantityByChildren(subItem, ratio);
+		}
+	}
+
+	@Transactional
+	public void computeItems(ActionRequest request, ActionResponse response) {
+		Map<String, Object> context = request.getRawContext();
+		Map<String, Object> parentLine = null;
+		Map<String, Object> updatedLine = null;
+
+		List<Map<String, Object>> mainItems = (List<Map<String, Object>>) context.get("items");
+
+		if (mainItems == null || mainItems.size() == 0)
+			return;
+
+		for (Map<String, Object> item : mainItems) {
+			ArrayList<Map<String, Object>> flatList = flattenItems(item);
+			for (Map<String, Object> _item : flatList) {
+				if (hasItemChanged(_item)) {
+					parentLine = item;
+					updatedLine = _item;
+					break;
+				}
+			}
+		}
+
+		if (updatedLine == null)
+			return;
+
+		String lineUpdateType = getItemProductUpdateType(updatedLine);
+
+		switch (lineUpdateType) {
+		case "parent":
+			updateQuantityByParent(parentLine);
+			break;
+		case "children":
+			updateQuantityByChildren(updatedLine, getItemQtyChangedRatio(updatedLine));
+			break;
+		}
+
+		response.setValue("items", mainItems);
+	}
+
+	public void onConfirm(ActionRequest request, ActionResponse response) {
+
+		Order order = request.getContext().asType(Order.class);
+
+		response.setReadonly("orderDate", order.getConfirmed());
+		response.setReadonly("confirmDate", order.getConfirmed());
+
+		if (order.getConfirmed() == Boolean.TRUE && order.getConfirmDate() == null) {
+			response.setValue("confirmDate", LocalDate.now());
+		}
+
+		if (order.getConfirmed() == Boolean.TRUE) {
+			response.setValue("status", OrderStatus.OPEN);
+		} else if (order.getStatus() == OrderStatus.OPEN) {
+			response.setValue("status", OrderStatus.DRAFT);
+		}
+	}
+
+	public void calculate(ActionRequest request, ActionResponse response) {
+
+		Order order = request.getContext().asType(Order.class);
+		order = service.calculate(order);
+
+		response.setValue("amount", order.getAmount());
+		response.setValue("taxAmount", order.getTaxAmount());
+		response.setValue("totalAmount", order.getTotalAmount());
+	}
+
+	public void reportToday(ActionRequest request, ActionResponse response) {
+		EntityManager em = getEntityManager();
+		Query q1 = em.createQuery("SELECT SUM(self.totalAmount) FROM Order AS self "
+				+ "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
+				+ "MONTH(self.orderDate) = MONTH(current_date) AND " + "DAY(self.orderDate) = DAY(current_date) - 1");
+
+		Query q2 = em.createQuery("SELECT SUM(self.totalAmount) FROM Order AS self "
+				+ "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
+				+ "MONTH(self.orderDate) = MONTH(current_date) AND " + "DAY(self.orderDate) = DAY(current_date)");
+
+		List<?> r1 = q1.getResultList();
+		BigDecimal last = r1.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r1.get(0);
+
+		List<?> r2 = q2.getResultList();
+		BigDecimal total = r2.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r2.get(0);
+
+		BigDecimal percent = BigDecimal.ZERO;
+		if (total.compareTo(BigDecimal.ZERO) == 1) {
+			percent = total.subtract(last).multiply(new BigDecimal(100)).divide(total, RoundingMode.HALF_UP);
+		}
+
+		Map<String, Object> data = new HashMap<>();
+		data.put("total", total);
+		data.put("percent", percent);
+		data.put("down", total.compareTo(last) == -1);
+
+		response.setData(Lists.newArrayList(data));
+	}
+
+	public void reportMonthly(ActionRequest request, ActionResponse response) {
+		EntityManager em = getEntityManager();
+		Query q1 = em.createQuery("SELECT SUM(self.totalAmount) FROM Order AS self "
+				+ "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
+				+ "MONTH(self.orderDate) = MONTH(current_date) - 1");
+
+		Query q2 = em.createQuery("SELECT SUM(self.totalAmount) FROM Order AS self "
+				+ "WHERE YEAR(self.orderDate) = YEAR(current_date) AND "
+				+ "MONTH(self.orderDate) = MONTH(current_date)");
+
+		List<?> r1 = q1.getResultList();
+		BigDecimal last = r1.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r1.get(0);
+
+		List<?> r2 = q2.getResultList();
+		BigDecimal total = r2.get(0) == null ? BigDecimal.ZERO : (BigDecimal) r2.get(0);
+
+		BigDecimal percent = BigDecimal.ZERO;
+		if (total.compareTo(BigDecimal.ZERO) == 1) {
+			percent = total.subtract(last).divide(total, 4, RoundingMode.HALF_UP);
+		}
+
+		Map<String, Object> data = new HashMap<>();
+		data.put("total", total);
+		data.put("percent", percent);
+		data.put("up", total.compareTo(last) > 0);
+		data.put("tag", I18n.get("Monthly"));
+		data.put("tagCss", "label-success");
+
+		response.setData(Lists.newArrayList(data));
+	}
+
+	public void showTotalSales(ActionRequest request, ActionResponse response) {
+		List<Map<String, Object>> data = (List<Map<String, Object>>) request.getRawContext().get("_data");
+		if (ObjectUtils.isEmpty(data)) {
+			response.setNotify(I18n.get("No sales"));
+			return;
+		}
+		BigDecimal totalAmount = data.stream().map(i -> i.get("amount").toString()).map(BigDecimal::new)
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+		response.setNotify(String.format("%s : %s", I18n.get("Total sales"), totalAmount));
+	}
+
+	public void showCustomerSales(ActionRequest request, ActionResponse response) {
+		Object data = request.getRawContext().get("customerId");
+		if (ObjectUtils.isEmpty(data)) {
+			return;
+		}
+
+		ActionView.ActionViewBuilder builder = ActionView.define("Customer sales").model(Order.class.getName());
+		builder.domain("self.customer.id = " + data);
+		response.setView(builder.map());
+	}
 }
